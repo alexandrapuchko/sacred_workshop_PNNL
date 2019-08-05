f530cd2be372fbb09aa4c487baecf17c133ed307
Alexandra Puchko <alexandra.puchko@gmail.com>
2019-08-05 09:54:00 -0400

    Update example.py

--- example.py
+++ example.py
@@ -23,15 +23,25 @@ def my_config():
 
 @ex.automain
 def run(C, gamma, _run):
+        #load a dataset
         iris = datasets.load_iris()
+        
+        #permute the dataset
         per = permutation(iris.target.size)
         iris.data = iris.data[per]
         iris.target = iris.target[per]
+        #create an SVM classifier with 'rbf' kernel
         clf = svm.SVC(C, 'rbf', gamma=gamma)
-
+        
+        #build train and dev set
         train_x, train_y = iris.data[:90], iris.target[:90]
         dev_x, dev_y = iris.data[90:], iris.target[90:]
 
+        #train classifier
         clf.fit(train_x, train_y)
+        
+        #get the score
         score = clf.score(dev_x, dev_y)
+        
+        #save the score
         _run.log_scalar('score', score)
